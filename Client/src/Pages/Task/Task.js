c5c14fd68dac985e139775053ec4a8d1aaa1bb52
anas <anasbadreddine.official@gmail.com>
2023-05-22 16:32:24 +0100

    add task page done

--- Client/src/Pages/Task/Task.js
+++ Client/src/Pages/Task/Task.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Task.css'
+import { Link } from 'react-router-dom'
 
 const Task = () => {
   return (
@@ -9,7 +10,35 @@ const Task = () => {
 
 const AddTask = () => {
     return(
-        <div>Add Task</div>
+        <>
+        <div className='ownbodyuser'>
+      <div className="wrapper">
+      <h2>Add Task</h2>
+      <form action="#">
+      <div className="input-box">
+        <input type="text" placeholder="Task Title" required/>
+      </div>
+      {/* 
+      <div className="input-box input_desc">
+       <input type="text" placeholder="Task Description" required/>
+      </div>*/}
+
+
+      <div className="input-box">
+       <textarea placeholder="Task Description" required/>
+      </div>
+      
+
+      <div className="input-box button">
+      <input type="Submit" value="Add Task"/>
+    </div>
+    <div className="text">
+      <h3><Link to='/showtasks'>Cancel</Link></h3>
+    </div>
+      </form>
+      </div>
+      </div> 
+        </>
     )
 }
 
